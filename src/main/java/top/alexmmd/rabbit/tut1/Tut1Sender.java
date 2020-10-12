5d0a1dacedd448974b87e7ed7b167967fa2867cb
wangyonghui <2930807240@qq.com>
2020-10-12 17:02:09 +0800

    完成第三个demo

--- src/main/java/top/alexmmd/rabbit/tut1/Tut1Sender.java
+++ src/main/java/top/alexmmd/rabbit/tut1/Tut1Sender.java
@@ -9,6 +9,8 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import java.util.Date;
+
 /**
  * @author Gary Russell
  * @author Scott Deeg
@@ -23,7 +25,7 @@ public class Tut1Sender {
 
     @Scheduled(fixedDelay = 1000, initialDelay = 500)
     public void send() {
-        String message = "Hello World!";
+        String message = "Hello World!" + new Date();
         this.template.convertAndSend(queue.getName(), message);
         System.out.println(" [x] Sent '" + message + "'");
     }
